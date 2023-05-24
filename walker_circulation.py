5b75e0e47200696582b88cc70692cbb996a16560
benjamino15 <benjaminweggelaar@hotmail.com>
2023-05-24 13:22:13 +0200

    new

--- walker_circulation.py
+++ walker_circulation.py
@@ -15,45 +15,6 @@ from tigramite import plotting as tp
 from tigramite.pcmci import PCMCI
 from tigramite.independence_tests.parcorr import ParCorr
 
-
-# to check tigramite version
-print(pkg_resources.get_distribution("tigramite").version)
-
-# Reading the data
-cpac = pd.read_csv('walker_data_obs/iera5_t2m_-150--130E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=20, header=None)
-epac = pd.read_csv('walker_data_obs/iera5_t2m_-100--80E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=20, header=None)
-wpac = pd.read_csv('walker_data_obs/iera5_slp_130-150E_-5-5N_n.dat', delimiter=r"\s+", skiprows=20, header=None)
-
-cpac = pd.read_csv('walker_data/icmip6_tas_mon_modmean_ssp126_-150--130E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=58, header=None)
-epac = pd.read_csv('walker_data/icmip6_tas_mon_modmean_ssp126_-100--80E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=58, header=None)
-wpac = pd.read_csv('walker_data/icmip6_psl_mon_modmean_ssp126_130-150E_-5-5N_n.dat', delimiter=r"\s+", skiprows=57, header=None)
-
-cpac.columns = ['Year', 'Jan', 'Feb', 'Mar', 'Apr', 'May','Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-epac.columns = cpac.columns
-wpac.columns = cpac.columns
-
-
-# stack observations into a single column
-cpac = cpac.drop(cpac.columns[0], axis=1)
-cpac = cpac.stack().reset_index(drop=True)
-
-epac = epac.drop(epac.columns[0], axis=1)
-epac = epac.stack().reset_index(drop=True)
-
-wpac = wpac.drop(wpac.columns[0], axis=1)
-wpac = wpac.stack().reset_index(drop=True)
-
-
-# specify date and add to dataframe
-date = pd.date_range(start='1950-01-01', end='2023-12-01', freq='MS')
-date = date.strftime('%Y-%m')
-
-df = pd.DataFrame({'Date': date, 'WPAC': wpac, 'CPAC': cpac, 'EPAC': epac})
-df = df[df['Date'] <= '2023-03']
-df = df[df['Date'] >= '1950-01']
-
-
-
 # function that extracts seasonal component from a simple additive decomposition model, and subracts
 # long term trend calculated with the Gaussian kernel smoother
 def deTrend_deSeasonalize(data, show_plot = True):
@@ -75,6 +36,43 @@ def deTrend_deSeasonalize(data, show_plot = True):
         result = data - smoothed
         return result
 
+
+
+# to check tigramite version
+print(pkg_resources.get_distribution("tigramite").version)
+
+# Reading the data
+cpac = pd.read_csv('walker_data_obs/iera5_t2m_-150--130E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=20, header=None)
+epac = pd.read_csv('walker_data_obs/iera5_t2m_-100--80E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=20, header=None)
+wpac = pd.read_csv('walker_data_obs/iera5_slp_130-150E_-5-5N_n.dat', delimiter=r"\s+", skiprows=20, header=None)
+
+cpac = pd.read_csv('CMIP6/CESM2/icmip6_tas_mon_CESM2_ssp585.0_-150--130E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=56, header=None)
+epac = pd.read_csv('CMIP6/CESM2/icmip6_tas_mon_CESM2_ssp585.0_-100--80E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=56, header=None)
+wpac = pd.read_csv('CMIP6/CESM2/icmip6_psl_mon_CESM2_ssp585.0_130-150E_-5-5N_n_su.dat', delimiter=r"\s+", skiprows=56, header=None)
+
+cpac.columns = ['Year', 'Jan', 'Feb', 'Mar', 'Apr', 'May','Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+epac.columns = cpac.columns
+wpac.columns = cpac.columns
+
+
+# stack observations into a single column
+cpac = cpac.drop(cpac.columns[0], axis=1)
+cpac = cpac.stack().reset_index(drop=True)
+
+epac = epac.drop(epac.columns[0], axis=1)
+epac = epac.stack().reset_index(drop=True)
+
+wpac = wpac.drop(wpac.columns[0], axis=1)
+wpac = wpac.stack().reset_index(drop=True)
+
+
+# specify date and add to dataframe
+date = pd.date_range(start='1850-01-01', end='2100-12-01', freq='MS')
+date = date.strftime('%Y-%m')
+
+df = pd.DataFrame({'Date': date, 'WPAC': wpac, 'CPAC': cpac, 'EPAC': epac})
+df = df[df['Date'] <= '2023-03']
+df = df[df['Date'] >= '1950-01']
 
 
 var_names = ['WPAC', 'CPAC', 'EPAC']
@@ -116,13 +114,13 @@ length = 12*(2023 - start_year) +3
 
 nino34 = np.loadtxt('iersst_nino3.4a.txt', skiprows=44)
 nino34= nino34[nino34[:,0] >= 1950]
-nino34= nino34[nino34[:,0] < 2023]
 nino34smoothed = pp.smooth(nino34[:,1], smooth_width=3, kernel='heaviside',
            mask=None, residuals=False)
 
-#plt.plot(nino34[:,1])
-#plt.plot(nino34smoothed)
-#plt.show()
+plt.plot(nino34[:,1])
+plt.plot(nino34smoothed)
+plt.show()
+
 
 # Construct mask for only neutral and La Nina phases
 #nino_mask = np.zeros(length)
@@ -149,9 +147,11 @@ nino34smoothed = pp.smooth(nino34[:,1], smooth_width=3, kernel='heaviside',
 
 df = pd.DataFrame({'WPAC': wpac, 'CPAC': cpac, 'EPAC': epac})
 
-
 # create tigramite dataframe to inspect
 dataframe = pp.DataFrame(df.values, datatime = {0:np.arange(len(df))}, var_names= var_names)
+
+# Diagnostic plots
+
 # tigramite data inspection
 # plot 1: timeseries
 tp.plot_timeseries(dataframe, color='black', show_meanline=True)
@@ -177,78 +177,98 @@ plt.show()
 lag_func_matrix = tp.plot_lagfuncs(val_matrix=correlations, setup_args={'var_names':var_names,
                                     'x_base':5, 'y_base':.5}); plt.show()
 
-# run PCMCI
+
+
+# Causal network 1: All observations
 pcmci.verbosity = 1
-results = pcmci.run_pcmci(tau_max=3, tau_min=1, pc_alpha=None, alpha_level=0.01, fdr_method = 'fdr_bh')
-
-# plot 5: causal graph
-tp.plot_graph(
-    val_matrix=results['val_matrix'],
-    graph=results['graph'],
-    var_names=var_names,
-    link_colorbar_label='cross-MCI',
-    node_colorbar_label='auto-MCI')
-plt.show()
-
-########################### Adding map to causal diagram ##################################
-
-import cartopy
-import cartopy.crs as ccrs
-
-
-
-# Create Plate Carrée projection
-proj = ccrs.Orthographic(central_longitude=-155.0,central_latitude=15.0)
-central_proj = ccrs.PlateCarree(central_longitude=0.0)
-
-# Create a figure and axis using the Plate Carrée projection
-fig, ax = plt.subplots(subplot_kw={'projection': proj})
-
-# Add coastlines to the axis
-ax.coastlines()
-ax.add_feature(cartopy.feature.LAND, zorder=-1, facecolor = 'lightgrey')
-ax.add_feature(cartopy.feature.OCEAN, zorder=-1, facecolor = 'white')
-ax.add_feature(cartopy.feature.COASTLINE, zorder=-1, linewidth=.5)
-#Add areas
-x, y = [130, 150, 150, 130], [-5, -5, 5, 5]
-ax.fill(x, y, transform=central_proj, color='coral', alpha=0.4, zorder=0)
-x, y = [-150, -140, -130, -120, -120, -130, -140, -150], [-5, -5, -5, -5, 5, 5, 5, 5]
-ax.fill(x, y, transform=central_proj, color='coral', alpha=0.4,zorder=0)
-x, y = [-100, -80, -80, -100], [-5, -5, 5, 5]
-ax.fill(x, y, transform=central_proj, color='coral', alpha=0.4, zorder=0)
-ax.set_global()
-ax.gridlines(linewidth=.2, zorder=0)
-
-
-# causal network plot
-
-node_pos = {
-    'y': np.array([0., 0., 0.]),
-    'x': np.array([-5000000., 1000000., 6000000.])
-}
-
-ax = tp.plot_graph(
-    fig_ax=(fig, ax),
-    graph = results['graph'],
-    node_pos=node_pos,
-    figsize=(10, 5),
-    val_matrix=results['val_matrix'],
-    #cmap_edges='RdBu_r',
-    #edge_ticks=.5,
-    #show_colorbar=False,
-    var_names=var_names,
-    arrow_linewidth= 4,
-    curved_radius=.35,
-    node_size=1000000.,
-    #node_label_size=8,
-    node_aspect= 1.,
-    #link_label_fontsize=0,
-    #label_fontsize=8,
-    node_colorbar_label='auto-MCI',
-    link_colorbar_label='cross-MCI')
-
-plt.show()
-
-##################################################################################
+pcmci = PCMCI(dataframe=dataframe, cond_ind_test=parcorr,verbosity=1)
+results1 = pcmci.run_pcmci(tau_max=6, tau_min=1, pc_alpha=0.05, alpha_level=0.05, fdr_method = 'fdr_bh')
 
 # Causal network 2: Spring barrier
+
+# Construct mask to exclude spring barrier
+cycle_length = 12
+mask = np.ones(df.shape, dtype='bool')
+exc_spring = [0, 4, 5, 6, 7, 8, 9, 10, 11]
+for i in exc_spring:
+    mask[i::cycle_length, :] = False
+
+dataframe = pp.DataFrame(np.copy(df), datatime = {0:np.arange(len(df))}, var_names= var_names, mask = mask)
+pcmci = PCMCI(dataframe=dataframe, cond_ind_test=parcorr,verbosity=1)
+results2 = pcmci.run_pcmci(tau_max=6, tau_min=1, pc_alpha=0.05, alpha_level=0.05, fdr_method = 'fdr_bh')
+
+# Causal network 3: Spring barrier towards La Niña
+
+nina_mask = np.ones(length, dtype = 'bool')
+for t in range(length):
+    if np.sum(nino34smoothed[max(0, t-2): min(length, t+3)] < -0.5) >= 5:
+        nina_mask[t] = False
+
+for t in range(mask.shape[0]):
+   if nina_mask[t] == True:
+        mask[t] = True
+
+dataframe = pp.DataFrame(np.copy(df), datatime = {0:np.arange(len(df))}, var_names= var_names, mask = mask)
+pcmci = PCMCI(dataframe=dataframe, cond_ind_test=parcorr,verbosity=1)
+results3 = pcmci.run_pcmci(tau_max=6, tau_min=1, pc_alpha=0.05, alpha_level=0.05, fdr_method = 'fdr_bh')
+
+# Causal network 4: Spring barrier towards El Niño
+mask = np.ones(df.shape, dtype='bool')
+exc_spring = [0, 4, 5, 6, 7, 8, 9, 10, 11]
+for i in exc_spring:
+    mask[i::cycle_length, :] = False
+
+nino_mask = np.ones(length, dtype = 'bool')
+for t in range(length):
+    if np.sum(nino34smoothed[max(0, t-2): min(length, t+3)] > -0.5) >= 5:
+        nino_mask[t] = False
+
+for t in range(mask.shape[0]):
+   if nino_mask[t] == True:
+        mask[t] = True
+
+dataframe = pp.DataFrame(np.copy(df), datatime = {0:np.arange(len(df))}, var_names= var_names, mask = mask)
+pcmci = PCMCI(dataframe=dataframe, cond_ind_test=parcorr,verbosity=1)
+results4 = pcmci.run_pcmci(tau_max=6, tau_min=1, pc_alpha=0.05, alpha_level=0.05, fdr_method = 'fdr_bh')
+
+# Overall graph plot
+
+
+fig, axes = plt.subplots(2, 2)
+
+tp.plot_graph(
+    val_matrix=results1['val_matrix'],
+    graph=results1['graph'],
+    var_names=var_names,
+    fig_ax= (fig, axes[0,0]))
+axes[0, 0].set_title('All observations')
+
+tp.plot_graph(
+    val_matrix=results2['val_matrix'],
+    graph=results2['graph'],
+    var_names=var_names,
+    link_colorbar_label='cross-MCI',
+    node_colorbar_label='auto-MCI',
+    fig_ax= (fig, axes[0,1]))
+axes[0, 1].set_title('Spring barrier')
+
+tp.plot_graph(
+    val_matrix=results3['val_matrix'],
+    graph=results3['graph'],
+    var_names=var_names,
+    link_colorbar_label='cross-MCI',
+    node_colorbar_label='auto-MCI',
+    fig_ax= (fig, axes[1,0]))
+axes[1, 0].set_title('Spring barrier towards La Niña')
+
+tp.plot_graph(
+    val_matrix=results4['val_matrix'],
+    graph=results4['graph'],
+    var_names=var_names,
+    link_colorbar_label='cross-MCI',
+    node_colorbar_label='auto-MCI',
+    fig_ax= (fig, axes[1,1]))
+axes[1, 1].set_title('Spring barrier towards El Niño')
+
+plt.subplots_adjust(hspace=0.4)
+plt.show()
